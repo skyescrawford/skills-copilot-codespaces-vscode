ef25480f2f30b3ad21d5f9bb255853fe8c7a5097
skyescrawford <68087051+skyescrawford@users.noreply.github.com>
2024-11-24 23:35:41 +0000

    add skills.js file

---
+++ skills.js
@@ -0,0 +1,10 @@
+function createTreePattern(height) {
+    for (let i = 1; i <= height; i++) {
+        let spaces = ' '.repeat(height - i);
+        let stars = '*'.repeat(2 * i - 1);
+        console.log(spaces + stars + spaces);
+    }
+}
+
+// Example usage:
+createTreePattern(4);
