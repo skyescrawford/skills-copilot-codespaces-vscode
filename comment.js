8c976a60cb868c90c735cc37e740837c559a3e48
skyescrawford <68087051+skyescrawford@users.noreply.github.com>
2024-11-24 23:52:36 +0000

    add comment.js file

---
+++ comment.js
@@ -0,0 +1,22 @@
+// create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// index
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// get comment data
+app.get('/comment', (req, res) => {
+  res.json({message: 'This is a comment data'});
+});
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
